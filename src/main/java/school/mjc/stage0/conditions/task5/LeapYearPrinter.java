595ab402b42d9f6ad1418ab94fb6334806b229e3
Sergey Tsoy <sergeytsoy740@gmail.com>
2023-07-11 16:50:04 +0600

    Module #4.1. Task #5. Switch

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        /*год должен быть кратен 400
+         или год должен быть кратен 4, но не 100.*/
+
+        boolean isLeap = false;
+        if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
+            isLeap = true;
+        }
+        System.out.println(isLeap);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -1,6 +1,32 @@
 package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
+    //1, 3, 5, 7, 8, 10, 12 - 31
+    //2 - 28
+    //4, 6, 9, 11
     public void amountOfDays(int month) {
+        switch (month) {
+            case (1):
+            case (3):
+            case (5):
+            case (7):
+            case (8):
+            case (10):
+            case (12):
+                System.out.println(31);
+                break;
+            case (2):
+                System.out.println(28);
+                break;
+            case (4):
+            case (6):
+            case (9):
+            case (11):
+                System.out.println(30);
+                break;
+            default:
+                System.out.println("wrong data!");
+                break;
+        }
     }
 }
